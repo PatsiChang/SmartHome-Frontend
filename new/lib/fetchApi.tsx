82d13b038620dc1c4a3496106c3bb9f8a1f14110
Patsi <patsipatsichang@gmail.com>
2024-04-01 16:23:57 +0100

    Base Skeleton - fetch API

--- new/lib/fetchApi.tsx
+++ new/lib/fetchApi.tsx
@@ -1,17 +1,38 @@
 import * as UserSessionApi from './userSessionApi';
 
-export const GET = "GET";
-export const POST = "POST";
-export const PUT = "PUT";
-export const DELETE = "DELETE";
+export type Action = "POST" | "GET" | "PUT" | "DELETE";
 
-export async function doFetch(method : "GET" | "POST" | "PUT" | "DELETE", url : string, data ?: any) {
-    if (method !== GET && typeof data === 'undefined') {
+export async function doFetch<T>(url: Parameters<typeof fetch>[0], method: Action, data?: T) {
+
+    if (method !== "GET" && (data === null || data === undefined)) {
         throw new Error("body is missing. Fail to fetch.");
     }
-
     UserSessionApi.hasLoggedIn();
-    // TODO set token to authorization header if logged in
-
-    // TODO fetch
+    // Todo: Set token to authorization header if logged in
+    const token = UserSessionApi.getSessionToken();
+    let request: RequestInit = {
+        method,
+        headers: {
+            // "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        }
+    };
+    if (method !== "GET") {
+        request.body = JSON.stringify(data);
+    }
+    const response = await fetch(url, request);
+    if (response.ok) {
+        if (response.headers.get("content-length") != null && response.headers.get("content-length") != "0") {
+            if (response.headers.get("content-type")?.includes("text/plain")) {
+                return await response.text();
+            } else {
+                return await response.json();
+            }
+        }
+    } else {
+        throw new Error("Failed to fetch data. HTTP status: " + response.status);
+    }
 }
+
+
+
