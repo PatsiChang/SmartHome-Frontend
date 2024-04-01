aa1f2c789026a8f24954e870dddd7a22c319c95f
Patsi <patsipatsichang@gmail.com>
2024-04-01 17:28:17 +0100

    Fix Return type and token for header

--- new/lib/fetchApi.tsx
+++ new/lib/fetchApi.tsx
@@ -2,32 +2,32 @@ import * as UserSessionApi from './userSessionApi';
 
 export type Action = "POST" | "GET" | "PUT" | "DELETE";
 
-export async function doFetch<T>(url: Parameters<typeof fetch>[0], method: Action, data?: T) {
+export async function doFetch<T>(url: Parameters<typeof fetch>[0], method: Action, data?: any) {
 
     if (method !== "GET" && (data === null || data === undefined)) {
         throw new Error("body is missing. Fail to fetch.");
     }
-    UserSessionApi.hasLoggedIn();
-    // Todo: Set token to authorization header if logged in
-    const token = UserSessionApi.getSessionToken();
     let request: RequestInit = {
         method,
         headers: {
             // "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
+            ...(UserSessionApi.hasLoggedIn() ? {
+                "Authorization": `Bearer ${UserSessionApi.getSessionToken()}`
+            } : {})
         }
     };
     if (method !== "GET") {
-        request.body = JSON.stringify(data);
+        if (typeof data !== "string") { request.body = JSON.stringify(data) }
+        else { request.body = data }
     }
     const response = await fetch(url, request);
     if (response.ok) {
-        if (response.headers.get("content-length") != null && response.headers.get("content-length") != "0") {
-            if (response.headers.get("content-type")?.includes("text/plain")) {
-                return await response.text();
-            } else {
-                return await response.json();
-            }
+        // response.headers.get("content-length") != null && response.headers.get("content-length") != "0"
+        if (response.headers.get("content-type")?.includes("application/json")) {
+            console.log("here2")
+            return await response.json() as T;
+        } else if (response.headers.get("content-type")?.includes("text/plain")) {
+            return await response.text();
         }
     } else {
         throw new Error("Failed to fetch data. HTTP status: " + response.status);
